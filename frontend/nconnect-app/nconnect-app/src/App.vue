739593a2cf0fe5ae417471b325ee1ab56dd599e4
Marek Jaroš <marekjarko90@gmail.com>
2024-04-21 21:38:46 +0200

    new: topbar

--- frontend/nconnect-app/nconnect-app/src/App.vue
+++ frontend/nconnect-app/nconnect-app/src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="navigation-area primary-shadow">
+    <div class="top-info-bar">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="contact-info-bar">
+              <ul>
+                <li><i class="fa fa-map-marker"></i> 49 West 32nd Street, New York</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-6 text-right mobile-left">
+            <div class="contact-info-bar">
+              <ul>
+                <li><i class="fa fa-phone"></i> +02 525 486 232</li>
+                <li><i class="fa fa-envelope"></i> eelias.cse@gmail.com</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div><!-- /.container -->
+    </div><!-- /.top-info-bar -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
