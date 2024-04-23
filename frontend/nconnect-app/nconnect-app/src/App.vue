d0ab1f5f0a57c57121f391e5098154a97848c4ef
Marek Jaroš <marekjarko90@gmail.com>
2024-04-23 10:26:24 +0200

    new: added index.html file into NavigationArea

--- frontend/nconnect-app/nconnect-app/src/App.vue
+++ frontend/nconnect-app/nconnect-app/src/App.vue
@@ -1,47 +1,30 @@
 <template>
-  <div class="navigation-area primary-shadow">
-    <div class="top-info-bar">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="contact-info-bar">
-              <ul>
-                <li><i class="fa fa-map-marker"></i> 49 West 32nd Street, New York</li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-sm-6 text-right mobile-left">
-            <div class="contact-info-bar">
-              <ul>
-                <li><i class="fa fa-phone"></i> +02 525 486 232</li>
-                <li><i class="fa fa-envelope"></i> eelias.cse@gmail.com</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div><!-- /.container -->
-    </div><!-- /.top-info-bar -->
-  </div>
+
+    <NavigationArea />
+
+
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
+import NavigationArea from "@/components/NavigationArea.vue";
+
 export default {
   name: 'App',
   components: {
+    NavigationArea
     // HelloWorld
   }
 }
 </script>
 
 <style>
+@import 'css/style.css';
+@import 'css/font.css';
+@import 'css/color.css';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
