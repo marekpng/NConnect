cfcd3abe0e041098df519f634d593cc63a66285b
Marek Jaroš <marekjarko90@gmail.com>
2024-04-23 13:25:34 +0200

    new: Index file copied

--- frontend/NConnect/src/router/index.js
+++ frontend/NConnect/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import IndexView from "@/views/IndexView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'index',
+      component: IndexView
     },
     {
       path: '/about',
