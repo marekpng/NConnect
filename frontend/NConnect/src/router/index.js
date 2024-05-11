e6b1e1bf3c147af5622b87f04bda033c62779226
Marek Jaroš <marekjarko90@gmail.com>
2024-05-11 18:01:16 +0200

    new: register new admin

--- frontend/NConnect/src/router/index.js
+++ frontend/NConnect/src/router/index.js
@@ -4,6 +4,7 @@ import IndexView from "@/views/IndexView.vue";
 import AdminView from "@/views/AdminView.vue";
 import AdminDashboardView from "@/views/AdminDashboardView.vue";
 import CrudStagesView from "@/views/CrudStagesView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +13,11 @@ const router = createRouter({
       path: '/',
       name: 'index',
       component: IndexView
+    },
+    {
+      path: '/admin/register',
+      name: 'register',
+      component: RegisterView
     },
     {
       path: '/login',
--- frontend/NConnect/src/views/AdminDashboardView.vue
+++ frontend/NConnect/src/views/AdminDashboardView.vue
@@ -5,16 +5,41 @@
       <ul>
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/admin/crud-stages">CRUD Stages</router-link></li>
+        <li><router-link to="/admin/register">Register new admin</router-link></li>
+
       </ul>
     </nav>
     <router-view></router-view>
+    <button @click="logout">Logout</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import router from "@/router/index.js";
+
 export default {
-  name: "AdminDashboardView"
+  name: "AdminDashboardView",
+  data() {
+    return {}
+  },
+  methods: {
+    logout() {
+      axios.post('http://127.0.0.1:8000/api/logout', {}, { headers: { Authorization: `Bearer ${localStorage.getItem('adminToken')}` } })
+          .then(response => {
+            console.log(response.data);
+            // Clear the token from localStorage
+            localStorage.removeItem('adminToken');
+            router.push({ name: 'index' });
+            // Optionally, redirect to login page or perform any other action after logout
+          })
+          .catch(error => {
+            console.error('Error logging out:', error);
+          });
+    }
+  }
 }
+
 </script>
 
 <style scoped>
